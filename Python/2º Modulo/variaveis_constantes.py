fafd54d8a8b92b9921ecae8a29e81e75f471a26c
Gabriel <bielfgq@gmail.com>
2023-09-13 21:41:39 -0300

    Conteúdos - Conhecendo a linguagem Python

---
+++ Python/2º Modulo/variaveis_constantes.py
@@ -0,0 +1,12 @@
+name = "Gabriel"
+idade = 20
+
+name, idade = "Vanessa", 43
+
+print(name, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
