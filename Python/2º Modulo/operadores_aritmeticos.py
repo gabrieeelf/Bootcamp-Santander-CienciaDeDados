10045bb5a2cca8237f69622e1f032d96ae4d2d15
Gabriel <bielfgq@gmail.com>
2023-09-19 20:21:40 -0300

    Novo conteúdo

---
+++ Python/2º Modulo/operadores_aritmeticos.py
@@ -0,0 +1,15 @@
+prod1 = 10
+prod2 = 20
+
+print(prod1 + prod2) #Adição
+print(prod1 - prod2) #Subtração
+print(prod1 / prod2) #Divisão
+print(prod1 // prod2) #Divisão com Resultado Inteiro
+print(prod1 * prod2) #Multiplicação
+print(prod1 % prod2) #Resto da Divisão
+print(prod1 ** prod2) #Exponeciação
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * (2 - 2) ** 2)
+print(x)
+print(y)
